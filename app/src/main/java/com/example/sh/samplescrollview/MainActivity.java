c7574f21f55f0e6dbfb77445c02a93e160217a86
lilo890 <lozalef@gmail.com>
2016-05-14 18:55:20 +0900

    Complete ScrollView  by LinearLayout

--- app/src/main/java/com/example/sh/samplescrollview/MainActivity.java
+++ app/src/main/java/com/example/sh/samplescrollview/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.sh.samplescrollview;
 
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ScrollView scrollView01;
+    ImageView imageView01;
+    BitmapDrawable bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scrollView01 = (ScrollView) findViewById(R.id.scrollView01);
+        imageView01 = (ImageView) findViewById(R.id.imageView01);
+        Button button01 = (Button) findViewById(R.id.Button01);
+
+        scrollView01.setHorizontalScrollBarEnabled(true);
+
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.image01);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView01.setImageDrawable(bitmap);
+        imageView01.getLayoutParams().width = bitmapWidth;
+        imageView01.getLayoutParams().height = bitmapHeight;
+    }
+
+    public void onButton1Clicked(View v){
+        changeImage();
+    }
+
+    private void changeImage(){
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.image02);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView01.setImageDrawable(bitmap);
+        imageView01.getLayoutParams().width = bitmapWidth;
+        imageView01.getLayoutParams().height = bitmapHeight;
+
     }
 }
